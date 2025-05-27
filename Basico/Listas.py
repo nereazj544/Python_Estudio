2692b023006be0d120db048394ae462858bab22e
nereazj544 <nzjvirtual@gmail.com>
2025-05-27 16:33:39 +0200

    Agregar lista con diferentes tipos de datos en Listas.py

--- Basico/Listas.py
+++ Basico/Listas.py
@@ -5,3 +5,7 @@ my_list_str = ["hola"]
 print("Lista de cadenas:", my_list_str)  # Imprime la lista de cadenas
 my_list_str.append("mundo")  # Agrega un nuevo elemento a la lista de cadenas
 print("Lista de cadenas después de agregar un elemento:", my_list_str)  # Imprime la lista de cadenas después de agregar un elemento
+
+my_other_list = [20, 1.53, "Neo"]
+print("Otra lista con diferentes tipos de datos:", my_other_list)  # Imprime otra lista con diferentes tipos de datos
+
