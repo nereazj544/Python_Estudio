427d9119c5eb991d1accf6b11d806b7c429de60a
nereazj544 <nzjvirtual@gmail.com>
2025-05-27 16:18:27 +0200

    Agregar archivo Listas.py con ejemplos de listas y manipulación de cadenas

---
+++ Basico/Listas.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 4, 5]  # Crea una lista de números
+print("Lista original:", my_list)  # Imprime la lista original
+
+my_list_str = ["hola"]
+print("Lista de cadenas:", my_list_str)  # Imprime la lista de cadenas
+my_list_str.append("mundo")  # Agrega un nuevo elemento a la lista de cadenas
+print("Lista de cadenas después de agregar un elemento:", my_list_str)  # Imprime la lista de cadenas después de agregar un elemento
