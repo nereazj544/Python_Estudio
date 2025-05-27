0f66836667e71101c4e9eb9130fe4b50fc9559ff
nereazj544 <nzjvirtual@gmail.com>
2025-05-27 15:47:29 +0200

    Agregar ejemplos de cadenas con tabulación y escape en Strings.py

--- Basico/Strings.py
+++ Basico/Strings.py
@@ -9,3 +9,10 @@ print(my_str + ", " + my_other_str)  # Concatena dos cadenas de texto
 
 my_salto_line = "Esta es una nueva línea\ncon salto de línea"  # Crea una cadena con un salto de línea
 print(my_salto_line)  # Imprime una cadena con un salto de línea
+
+
+my_tab_line = "\tEsta es una nueva línea con tabulación"  # Crea una cadena con un tabulador
+print(my_tab_line)  # Imprime una cadena con un tabulador
+
+my_scape_str = "\\t Este es un String \\n escapado"
+print(my_scape_str)  # Imprime una cadena con un salto de línea y un tabulador
