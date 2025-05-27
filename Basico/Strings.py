3fdf13ad599465a309f4028ab9c32065ed67c4f3
nereazj544 <nzjvirtual@gmail.com>
2025-05-27 15:25:35 +0200

    Agregar archivo Strings.py con ejemplos de manipulación de cadenas

---
+++ Basico/Strings.py
@@ -0,0 +1,11 @@
+print('Longitud de una cadena')
+
+my_str = "Hola, Mundo!"
+my_other_str = "mi otra cadena de texto"
+
+print(f"Longitud de la cadena '{my_str}': {len(my_str)}")  # Imprime la longitud de la cadena
+
+print(my_str + ", " + my_other_str)  # Concatena dos cadenas de texto
+
+my_salto_line = "Esta es una nueva línea\ncon salto de línea"  # Crea una cadena con un salto de línea
+print(my_salto_line)  # Imprime una cadena con un salto de línea
