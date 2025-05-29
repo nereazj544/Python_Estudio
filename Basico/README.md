4b74875f90dce1c27a38a4ec7e5fd4415fb6f23c
nereazj544 <nzjvirtual@gmail.com>
2025-05-29 09:49:58 +0200

    Actualizar README.md con ejemplos de listas y tuplas; agregar manipulación de tuplas en Tuplas.py; crear archivo Set.py vacío.

--- Basico/README.md
+++ Basico/README.md
@@ -11,3 +11,12 @@
 
 La principal diferencia entre una lista y una tupla es que las listas son mutables (pueden cambiar) y las tuplas son inmutables (no pueden cambiar una vez creadas).
 
+Se pueden cambiar los tipos de las listas y tuplas.
+
+```python
+my_list = [1, 2, 3]
+my_tuple = (1, 2, 3)
+
+my_list = list(my_tuple)
+my_tuple = tuple(my_list)
+```
--- Basico/Tuplas.py
+++ Basico/Tuplas.py
@@ -14,3 +14,17 @@ print(my_tuple.index("Neo"))  # Busca el índice del elemento "Neo" en la tupla
 
 sum_tuple = my_other_tuple + my_tuple
 print(sum_tuple)
+print(sum_tuple[5:7])
+
+# Convertir una tupla a mutable lista
+my_list = list(my_tuple)
+print(type(my_list))  # Convierte la tupla a una lista y la imprime
+
+
+my_list.insert(0, "Kurai")
+print(my_list)  # Imprime la lista después de modificarla
+my_tuple = tuple(my_list)
+print(my_tuple)  # Convierte la lista de nuevo a tupla y la imprime
+print(type(my_tuple))  # Imprime el tipo de la tupla después de la conversión
+
+# del my_tuple  # Elimina la variable
