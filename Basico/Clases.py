746990416eda3aefbb060cf4064e83899f7b026b
nereazj544 <nzjvirtual@gmail.com>
2025-05-29 14:41:52 +0200

    Actualizar la clase Persona para incluir apellido y alias; modificar el método saludar y la creación de instancias.

--- Basico/Clases.py
+++ Basico/Clases.py
@@ -1,17 +1,35 @@
 ## CLASES
 
 class Persona:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
+    #esto es como un constructor en otros lenguajes
+    # __init__ es un método especial que se llama al crear una instancia de la clase
+    def __init__(self, nombre, surname, alias = "Sin alias"):
+        # self.nombre = nombre
+        # self.edad = edad
+
+        self.full_name = f"{nombre} {surname} {alias}"
 
     def saludar(self):
-        print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años.")
+        print(f"Hola, {self.full_name}.")
+
+    def sentado(self):
+        print(f"{self.full_name} está sentado.")
 
 # Crear una instancia de la clase Persona
 name = input("Introduce el nombre de la persona: ")
-age = int(input("Introduce la edad de la persona: "))
+surname = input("Introduce el apellido de la persona: ")
+alias = input("Introduce el alias de la persona (opcional): ")
+# age = int(input("Introduce la edad de la persona: "))
 
-persona = Persona(name, age)
+
+persona = Persona(name, surname, alias)
+print(persona.full_name)  # Imprime el nombre completo de la persona
 # Llamar al método saludar
 persona.saludar()
+persona.sentado()
+
+my_other_persona = Persona("Juan", "Pérez", "Juampi")
+print(my_other_persona.full_name)  # Imprime el nombre completo de la persona
+# Llamar al método saludar
+my_other_persona.saludar()
+my_other_persona.sentado()
