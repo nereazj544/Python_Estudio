1031c924f89bbefaacce25bb3d6e93088837eb85
nereazj544 <nzjvirtual@gmail.com>
2025-05-29 15:16:07 +0200

    Agregar instancia de la clase Persona y llamar al método saludar en modulos.py

--- Basico/modulos.py
+++ Basico/modulos.py
@@ -1,2 +1,14 @@
 # Modules
 
+#importar modulos de clases
+import Clases
+# Importar la clase Persona desde el módulo Clases
+from Clases import Persona
+
+
+#no es como java o c# que tienes que hacer: persona p = new Persona(); se crea directamente la instancia
+# Crear una instancia de la clase Persona
+p = Persona("Carlos", "Gómez", "Carlitos")  
+p.saludar()  # Llamar al método saludar de la instancia p
+
+
