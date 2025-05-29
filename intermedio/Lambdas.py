3c9dd82ad19d05abe669bcc5310fff2af3bb9dbb
nereazj544 <nzjvirtual@gmail.com>
2025-05-29 15:56:53 +0200

    Agregar implementación inicial de funciones lambda en Lambdas.py

---
+++ intermedio/Lambdas.py
@@ -0,0 +1,11 @@
+# Lambdas
+# Definicion de lambdas
+# Las funciones lambda son funciones anónimas que se definen en una sola línea.
+# Se utilizan principalmente para operaciones simples y se pueden pasar como argumentos a otras funciones.
+# Son útiles cuando se necesita una función pequeña y no se quiere definir una función completa con def.
+
+
+lambda fist, second: fist + second
+
+print((lambda first, second: first + second)(5, 10))  # Llamada a la función lambda
+
--- intermedio/listCompren.py
+++ intermedio/listCompren.py
@@ -1,1 +1,9 @@
 # list comprehension
+
+my_list_org = [0, 1, 2, 3, 4, 5]
+print(my_list_org)
+
+my_list_comprehension = [i * 2 for i in range(14)]
+print(my_list_comprehension)
+my_list_comprehension = [i for i in range(14)]
+print(my_list_comprehension)
