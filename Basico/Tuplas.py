25524f5f39d4ad6fd79189c84347fd2d41c00cce
nereazj544 <nzjvirtual@gmail.com>
2025-05-29 09:38:34 +0200

    Agregar tupla my_other_tuple con valores iniciales y sumar con my_tuple

--- Basico/Tuplas.py
+++ Basico/Tuplas.py
@@ -1,7 +1,7 @@
 # TUPLAS = ()
 
 my_tuple = tuple()  # Crea una tupla vacía
-my_other_tuple = ()
+my_other_tuple = (2, 13, 290)
 
 my_tuple=(20, 1.53, "Neo", "Kurai")  # Crea una tupla con diferentes tipos de datos
 print(type(my_tuple))  # Imprime el tipo de la tupla
@@ -12,3 +12,5 @@ print(my_tuple[-1])
 print(my_tuple.count("Neo"))  # Cuenta cuántas veces aparece "Neo" en la tupla
 print(my_tuple.index("Neo"))  # Busca el índice del elemento "Neo" en la tupla
 
+sum_tuple = my_other_tuple + my_tuple
+print(sum_tuple)
---
+++ Readme.md
@@ -0,0 +1,1 @@
+https://youtu.be/Kp4Mvapo5kc
