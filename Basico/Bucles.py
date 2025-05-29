1ad594665f16c33fb586aff4ee0fa6771af45dac
nereazj544 <nzjvirtual@gmail.com>
2025-05-29 14:05:24 +0200

    Actualizar Condicionales.py para mejorar la evaluación de entrada de usuario y agregar comparaciones de temperatura

---
+++ Basico/Bucles.py
@@ -0,0 +1,39 @@
+## Bucles ##
+
+# Bucle for
+for i in range(10):
+    print(i)
+
+
+# Bucle while
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+
+# Bucle for con lista
+my_list = [1, 2, 3, 4, 5]
+for item in my_list:
+    print(item)
+
+
+# Bucle for con cadena
+my_string = "Hola"
+for char in my_string:
+    print(char)
+
+
+
+# Bucle for con diccionario
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+for key, value in my_dict.items():
+    print(f"Clave: {key}, Valor: {value}")
+
+
+# Bucle for con enumerate
+my_list = ['a', 'b', 'c']
+for index, value in enumerate(my_list):
+    print(f"Índice: {index}, Valor: {value}")
+
+    
--- Basico/Condicionales.py
+++ Basico/Condicionales.py
@@ -4,12 +4,38 @@ input_value = input("Escribe True o False: ")
 
 
 
-if input_value == "True":
+if input_value == "True" or input_value == "true":
     print("La condición es verdadera")
-elif input_value == "False":
+elif input_value == "False" or input_value == "false":
     print("La condición es falsa")
 else:
     print("La condición es desconocida")
 
+input_value_temp = input("Escribe una temperatura: ")
+
+if input_value_temp > "0":
+    print(f"la temperatura es positiva:{input_value_temp}")
+elif input_value_temp < "0":
+    print(f"la temperatura es negativa:{input_value_temp}")
+else:
+    print(f"la temperatura es cero:{input_value_temp}")
 
 
+
+
+my_string = "Hola Mundo"
+my_string_empty = ""
+
+# Por defecto los if son True 
+
+print("my_string, comparacion con if")
+if my_string: # Si la cadena no está vacía, se evalúa como True
+    print("La cadena no está vacía")
+else:
+    print("La cadena está vacía")
+
+print("my_string_empty, comparacion con if")
+if my_string_empty:
+    print("La cadena no está vacía")
+else:
+    print("La cadena está vacía")
