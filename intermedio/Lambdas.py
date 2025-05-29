3c385a9a0412dc26144c5b4ad30ec641591fe353
nereazj544 <nzjvirtual@gmail.com>
2025-05-29 15:58:16 +0200

    Descomentar la definición de la función lambda y agregar llamada de prueba en Lambdas.py

--- intermedio/Lambdas.py
+++ intermedio/Lambdas.py
@@ -5,7 +5,10 @@
 # Son útiles cuando se necesita una función pequeña y no se quiere definir una función completa con def.
 
 
-lambda fist, second: fist + second
+# lambda fist, second: fist + second
+sum = lambda first, second: first + second  # Definición de una función lambda que suma dos números
+
+print(sum(5, 13))  # Llamada a la función lambda con argumentos 5 y 13
 
 print((lambda first, second: first + second)(5, 10))  # Llamada a la función lambda
 
