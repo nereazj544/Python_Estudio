0b6cb089d2c0133c835179a6e5a30df29235981b
nereazj544 <nzjvirtual@gmail.com>
2025-05-29 16:45:12 +0200

    Agregar implementación de closures en Funciones.py

--- intermedio/Funciones.py
+++ intermedio/Funciones.py
@@ -15,3 +15,15 @@ def sum_values (first, second):
     return sum_one(first + second)  # Llamada a la función pasada como argumento
 
 print(sum_values(5, 10))  # Llamada a la función que suma dos números y luego aplica sum_one
+
+## Closures ##
+# Un closure es una función que recuerda el entorno en el que fue creada, incluso después de que ese entorno haya sido destruido.
+
+def ten ():
+    def add(value):
+        return value + 10
+    return add  # Devuelve la función add, que es un closure
+
+print(ten()(3))  # Llamada al closure con el valor 3, que devuelve 13
+add_ten = ten()  # Llama a la función ten y obtiene el closure add
+print(add_ten(5))  # Llamada al closure con el valor 5, que devuelve 15
