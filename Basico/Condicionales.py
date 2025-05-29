f3cc1de9d157e999a7f0e70f1e2af86f760dc409
nereazj544 <nzjvirtual@gmail.com>
2025-05-29 13:38:38 +0200

    Agregar lógica de condicionales para evaluar entrada de usuario en Condicionales.py

---
+++ Basico/Condicionales.py
@@ -0,0 +1,15 @@
+## Condicionales
+print("True o False")
+input_value = input("Escribe True o False: ")
+
+
+
+if input_value == "True":
+    print("La condición es verdadera")
+elif input_value == "False":
+    print("La condición es falsa")
+else:
+    print("La condición es desconocida")
+
+
+
