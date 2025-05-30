a84c71362c7d094baeeef6c9a98fd448fcc107cf
nereazj544 <nzjvirtual@gmail.com>
2025-05-30 10:02:59 +0200

    Agregar lectura y escritura de ficheros en Ficheros.py y crear my_file2.txt con contenido "Hola mundo"

--- intermedio/Ficheros.py
+++ intermedio/Ficheros.py
@@ -6,7 +6,19 @@ import os
 
 text_file = open("intermedio/doc/my_file2.txt", "w") # Crear un fichero
 text_file_read = open("intermedio/doc/my_file.txt", "r") # Abrir un fichero para leer
-print(text_file_read.read()) # Leer el fichero
+#print(text_file_read.read()) # Leer el fichero
+#print(text_file_read.read(10)) # Leer los primeros 10 caracteres del fichero
+# print(text_file_read.readline()) # Leer la primera línea del fichero
+# print(text_file_read.readlines()) # Leer todas las líneas del fichero
+
+for line in text_file_read.readlines():
+    print(line.strip())  # Imprimir cada línea sin saltos de línea
+
+text_file.write("Hola mundo\n")
+
+os.remove("intermedio/doc/my_file2.txt")  
+# Eliminar el fichero creado
+
 
 """
 - r+ > leer y escribir
