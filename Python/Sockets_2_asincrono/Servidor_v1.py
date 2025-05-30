93b3d3a76d8b962cea837e3ba988fef0451f4c3b
nereazj544 <nzjvirtual@gmail.com>
2025-05-30 13:49:54 +0200

    Agregar manejo de conexión y recepción de mensajes en el servidor asíncrono, mejorando la interacción con el cliente y la gestión de logs.

--- Python/Sockets_2_asincrono/Servidor_v1.py
+++ Python/Sockets_2_asincrono/Servidor_v1.py
@@ -5,8 +5,37 @@
 import asyncio
 import socket
 
-from Log import Setup_servidor
-Setup_servidor()  # Configuración del logger
+from Log import server_log, server_error, server_warning, server_debug, server_critical
 
+HOST = "127.0.0.1"
+PORT = 6000
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Crea un socket TCP/IP
+server.bind((HOST, PORT))
+server.listen(5)  # Escucha conexiones entrantes
+
+async def task_client():
+    server_log(f"Start server v1 connectes to {HOST}:{PORT}")
+    try:
+        while True:
+            conn, addr = server.accept()
+            server_log(f"Connected to {addr}")
+            server_log("Waiting for data from client...")
+            print("> Esperando datos del cliente...")
+
+            while True:
+                msg = conn.recv(1024).decode()
+                if not msg:
+                    conn.close()
+                    print(f"> Conexión cerrada por {addr}")
+                    server_log(f"Connection closed by {addr}")
+                    break
+
+                print(f"[{addr}] {msg}")
+                server_log(f"[{addr}]")
+                msg = input("Escribe una respuesta: ")
+                conn.sendall(msg.encode())
+    except socket.error as e:
+        server_error(f"Error in task_client: {e}")
+        print(f"> Error en la tarea del cliente: {e}")
 
--- Python/Sockets_2_asincrono/log.py
+++ Python/Sockets_2_asincrono/log.py
@@ -3,19 +3,49 @@ import logging
 def Setup_servidor():
     logging.basicConfig(filename="Python/Sockets_2_asincrono/log/info_servidor.log", level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    logging.debug("[DEBUG]: ")
-    logging.info("[INFO]: ")
-    logging.warning("[WARNING]: ")
-    logging.error("[ERROR]: ")
-    logging.critical("[CRITICAL]: ")
-    logging.info("Logging setup complete.")
 
 def setup_cliente():
     logging.basicConfig(filename="Python/Sockets_2_asincrono/log/info_cliente.log", level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    logging.debug("[DEBUG]: ")
-    logging.info("[INFO]: ")
-    logging.warning("[WARNING]: ")
-    logging.error("[ERROR]: ")
-    logging.critical("[CRITICAL]: ")
-    logging.info("Logging setup complete.")
+
+def server_log(message):
+    Setup_servidor()
+    logging.info(f"[SERVER]: {message}")
+
+def client_log(message):
+    setup_cliente()
+    logging.info(f"[CLIENT]: {message}")
+
+
+def server_error(message):
+    Setup_servidor()
+    logging.error(f"[SERVER]: {message}")
+
+def client_error(message):
+    setup_cliente()
+    logging.error(f"[CLIENT]: {message}")
+
+def server_warning(message):
+    Setup_servidor()
+    logging.warning(f"[SERVER]: {message}")
+
+def client_warning(message):
+    setup_cliente()
+    logging.warning(f"[CLIENT]: {message}")
+
+
+def server_debug(message):
+    Setup_servidor()
+    logging.debug(f"[SERVER]: {message}")
+
+def client_debug(message):
+    setup_cliente()
+    logging.debug(f"[CLIENT]: {message}")
+
+def server_critical(message):
+    Setup_servidor()
+    logging.critical(f"[SERVER]: {message}")
+
+def client_critical(message):
+    setup_cliente()
+    logging.critical(f"[CLIENT]: {message}")
