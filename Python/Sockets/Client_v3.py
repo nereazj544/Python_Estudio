f78f9db745259d5bfb9d930761f1e80bfd33fb15
nereazj544 <nzjvirtual@gmail.com>
2025-05-30 12:11:25 +0200

    Corregir comparación de salida en Client_v3.py para que sea insensible a mayúsculas

--- Python/Sockets/Client_v3.py
+++ Python/Sockets/Client_v3.py
@@ -10,7 +10,7 @@ while "Exit":
     # Bucle para enviar mensajes hasta que se envíe "Exit"
     msg = input("Escribe un mensaje (o 'e x i t' para salir): ")  # Solicita al usuario un mensaje
     s.sendall(msg.encode())  # Envía el mensaje al servidor
-    if msg == "Exit":  # Si el usuario escribe "Exit", se sale del bucle
+    if msg.lower() == "exit":  # Si el usuario escribe "Exit", se sale del bucle
         break
     s.sendall(msg.encode())  # Envía el mensaje al servidor
 
